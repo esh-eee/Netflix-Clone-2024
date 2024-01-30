3045fe838b15e81d06b76c8c3ef430b58054df78
eshetu <assefaeshetu1221@gmail.com>
2024-01-30 05:17:21 +0300

    Banner

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-
+import Banner from '../../components/Banner/Banner'
 const Home = () => {
   return (
     <>  
     {/* making empty fragment not to take the defaukt property of div */}
         <Header/>
+        <Banner/>
         <Footer/>
     </>
   )
--- netflix-clone/src/utiles/axios.js
+++ netflix-clone/src/utiles/axios.js
@@ -1,8 +1,5 @@
-import React from 'react'
-
-const instance =axios.create ({
-  baseURL: "https://api.themoviedb.org/3",
-  
+import axios from "axios";
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
 });
-
 export default instance;
